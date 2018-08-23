7607d48630fd09ecc9e8fa7ec772663f7771c0bb
Raphaël Jammart <rja@maj.consulting>
2018-08-23 17:03:30 +0200

    add bundles config

--- stencil.config.ts
+++ stencil.config.ts
@@ -4,10 +4,7 @@ import { sass } from '@stencil/sass';
 export const config: Config = {
   namespace: 'ionicpwaelements',
   bundles: [
-    { components: ['ion-icon', 'ion-action-sheet-controller', 'ion-action-sheet',
-                   'ion-modal-controller', 'ion-modal', 'ion-animation-controller',
-                   'ion-toast-controller', 'ion-toast'] },
-    { components: ['ion-pwa-camera-modal', 'ion-pwa-camera', 'ion-pwa-action-sheet'] }
+    { components: ['ion-pwa-camera-modal', 'ion-pwa-camera', 'ion-pwa-action-sheet', 'ion-icon', 'ion-action-sheet-controller', 'ion-action-sheet', 'ion-modal-controller', 'ion-modal', 'ion-animation-controller', 'ion-toast-controller', 'ion-toast'] }
   ],
   plugins: [
     sass()
