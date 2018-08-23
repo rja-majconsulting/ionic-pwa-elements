db71289b018d290edcd994e656aa3b0dbb591d8b
Raphaël Jammart <rja@maj.consulting>
2018-08-23 15:05:34 +0200

    change to current version

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,6 +3,12 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'ionicpwaelements',
+  bundles: [
+    { components: ['ion-icon', 'ion-action-sheet-controller', 'ion-action-sheet',
+                   'ion-modal-controller', 'ion-modal', 'ion-animation-controller',
+                   'ion-toast-controller', 'ion-toast'] },
+    { components: ['ion-pwa-camera-modal', 'ion-pwa-camera', 'ion-pwa-action-sheet'] }
+  ],
   plugins: [
     sass()
   ],
